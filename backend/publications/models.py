ae97a18243a76b9ef9a3834ef0fab4f20c29cca7
Bryan M. Wolf <196235544+bryanmichaelwolf@users.noreply.github.com>
2026-05-20 14:26:12 -0700

    added organization and publication models

--- backend/publications/models.py
+++ backend/publications/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Organization(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+class Publication(models.Model):
+    organization = models.ForeignKey(
+        Organization,
+        on_delete=models.CASCADE,
+        related_name='publications'
+    )
+
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+
+    description = models.TextField(blank=True)
+
+    is_active = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
