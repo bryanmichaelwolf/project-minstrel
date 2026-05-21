22453b103453b035013922797473542c431847cf
Bryan M. Wolf <196235544+bryanmichaelwolf@users.noreply.github.com>
2026-05-21 06:03:22 -0700

    minor adjustments to data arrangement

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -24,11 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = os.getenv('DEBUG')
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = []
 
