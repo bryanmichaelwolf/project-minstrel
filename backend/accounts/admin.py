70bb69c699086520a2bd5d37c931724afabc0628
Bryan M. Wolf <196235544+bryanmichaelwolf@users.noreply.github.com>
2026-05-21 13:16:07 -0700

    registered new custom user model to admin

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from accounts.models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+
+    list_display = (
+        'id',
+        'email',
+        'username',
+        'is_staff',
+        'is_active',
+    )
+
+    ordering = ('email',)
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            'Custom Fields',
+            {
+                'fields': ()
+            },
+        ),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            'Custom Fields',
+            {
+                'fields': ()
+            },
+        ),
+    )
+    
