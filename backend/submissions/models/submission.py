b5e80a937096d233399fd3c7cc23ad16a4fb2458
Bryan M. Wolf <196235544+bryanmichaelwolf@users.noreply.github.com>
2026-05-23 05:21:49 -0700

    removed SubmissionStatus

--- backend/submissions/models/submission.py
+++ backend/submissions/models/submission.py
@@ -3,14 +3,6 @@ from django.conf import settings
 from publications.models import Publication
 
 
-class SubmissionStatus(models.TextChoices):
-    SUBMITTED = 'submitted', 'Submitted'
-    IN_REVIEW = 'in review', 'In Review'
-    ACCEPTED = 'accepted', 'Accepted'
-    REJECTED = 'rejected', 'Rejected'
-    WITHDRAWN = 'withdrawn', 'Withdrawn'
-
-
 class Submission(models.Model):
     publication = models.ForeignKey(
         Publication,
