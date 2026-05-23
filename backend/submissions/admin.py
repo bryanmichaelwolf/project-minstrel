a983007416abc4fd348bc91249a8c7b4ad50f4ab
Bryan M. Wolf <196235544+bryanmichaelwolf@users.noreply.github.com>
2026-05-23 05:09:58 -0700

    registered SubmissionEvents model

--- backend/submissions/admin.py
+++ backend/submissions/admin.py
@@ -1,5 +1,28 @@
 from django.contrib import admin
-from .models import Submission
+from .models import (
+    Submission,
+    SubmissionEvent,
+)
 
 
 admin.site.register(Submission)
+
+@admin.register(SubmissionEvent)
+class SubmissionEventAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'id',
+        'submission',
+        'event_type',
+        'actor',
+        'created_at',
+    )
+
+    list_filter = (
+        'event_type',
+    )
+
+    search_fields = (
+        'submission__title',
+        'actor__email',
+    )
