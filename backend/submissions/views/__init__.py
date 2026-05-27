3891707747e7d8b9d5e37499a93f9952d16512f3
Bryan M. Wolf <196235544+bryanmichaelwolf@users.noreply.github.com>
2026-05-27 14:36:37 -0700

    fixed typos in imports

--- backend/submissions/views/__init__.py
+++ backend/submissions/views/__init__.py
@@ -1,4 +1,4 @@
 from .submission_views import SubmissionViewSet
-from submission_event_views import SubmissionEventViewSet
+from .submission_event_views import SubmissionEventViewSet
 from .review_views import ReviewViewSet
 from .editorial_note_views import EditorialNoteViewSet
